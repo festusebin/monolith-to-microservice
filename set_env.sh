3359894c3d9635b2e34f003502a135e8ddb1301b
Festus Ebin <ebinfestus@gmail.com>
2021-06-25 10:13:00 +0100

    updated

---
+++ set_env.sh
@@ -0,0 +1,11 @@
+# This file is used for convenience of local development.
+# DO NOT STORE YOUR CREDENTIALS INTO GIT
+export POSTGRES_USERNAME=udagramfestusebindev
+export POSTGRES_PASSWORD=udagramfestusebindev
+export POSTGRES_HOST=udagramfestusebindev.ckgvyka93igu.us-east-2.rds.amazonaws.com
+export POSTGRES_DB=udagramfestusebindev
+export AWS_BUCKET=festus-777649253130-dev
+export AWS_REGION=us-east-2
+export AWS_PROFILE=default
+export JWT_SECRET=hello
+export URL=http://localhost:8100
